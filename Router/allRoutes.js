92bedbab5e9a34c8bcfcac71434699d72e3badc8
mel295 <melissa.bechu@outlook.fr>
2025-07-14 11:20:59 +0200

    install bootstrap

---
+++ Router/allRoutes.js
@@ -0,0 +1,10 @@
+import Route from "/Route.js";
+
+//Définir vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),];
+
+
+//le titre s'affiche comme ceci : Route.titre - websitename
+export const websitename = "Quai Antique";
+
